eb3dc4668e576560e43a941a36a9137371292ff6
Anmol Sharma <dev.anmolsharma@gmail.com>
2021-10-26 21:21:21 +0530

    adding views, url patterns, migrations, models, installed apps

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# creating my first view
+def index(request):
+    return HttpResponse('<h1>Hello, World. You are at polls index.</h1>')
